9584c747d84c319864f2dfbd787036777ac9995d
toeger <toeger@anonymouse.me>
2015-11-01 00:50:17 +0100

    Added support for emplacing components into entities Added moves Updated TODO

--- entity.h
+++ entity.h
@@ -17,95 +17,134 @@ template<class H, class... T>
 struct System_iterator;
 constexpr Impl::Id max_id = std::numeric_limits<Impl::Id>::max();
 
-struct System{
-    template<class T>
-    using remove_cvr = std::remove_cv_t<std::remove_reference_t<T>>;
+#if 0
+//causes a segfault on exit, don't know why
+namespace System{
+	template<class T>
+	using remove_cvr = std::remove_cv_t<std::remove_reference_t<T>>;
 
-    template<class Component>
-    static std::vector<remove_cvr<Component>> &get_components(){
-        return p_get_components<remove_cvr<Component>>();
-    }
-    template<class Component>
-    static std::vector<Impl::Id> &get_ids(){
-        return p_get_ids<remove_cvr<Component>>();
-    }
-    template<class... Components>
-    static System_iterator<Components...> range(){
-        System_iterator<Components...> si;
-        si.advance(0);
-        return si;
-    }
+	template<class Component>
+	static std::vector<Component> components{};
+	template<class Component>
+	static std::vector<Impl::Id> ids{max_id};
+
+	template<class Component>
+	std::vector<remove_cvr<Component>> &get_components(){
+		return components<remove_cvr<Component>>;
+	}
+	template<class Component>
+	std::vector<Impl::Id> &get_ids(){
+		return ids<remove_cvr<Component>>;
+	}
+	template<class... Components>
+	System_iterator<Components...> range(){
+		System_iterator<Components...> si;
+		si.advance(0);
+		return si;
+	}
+}
+
+#else
+
+struct System{
+	template<class T>
+	using remove_cvr = std::remove_cv_t<std::remove_reference_t<T>>;
+
+	template<class Component>
+	static std::vector<remove_cvr<Component>> &get_components(){
+		return p_get_components<remove_cvr<Component>>();
+	}
+	template<class Component>
+	static std::vector<Impl::Id> &get_ids(){
+		return p_get_ids<remove_cvr<Component>>();
+	}
+	template<class... Components>
+	static System_iterator<Components...> range(){
+		System_iterator<Components...> si;
+		si.advance(0);
+		return si;
+	}
 private:
-    template<class Component>
-    static std::vector<Component> &p_get_components(){
-        static std::vector<Component> components;
-        return components;
-    }
-    template<class Component>
-    static std::vector<Impl::Id> &p_get_ids(){
-        static std::vector<Impl::Id> ids = {max_id};
-        return ids;
-    }
+	template<class Component>
+	static std::vector<Component> &p_get_components(){
+		static std::vector<Component> components;
+		return components;
+	}
+	template<class Component>
+	static std::vector<Impl::Id> &p_get_ids(){
+		static std::vector<Impl::Id> ids = {max_id};
+		return ids;
+	}
 };
+
+#endif
 
 template <class T>
 struct System_iterator<T>{
-    Impl::Id advance(){
-        if (System::get_ids<T>()[current_index] == max_id)
-            return max_id;
-        return advance(System::get_ids<T>()[current_index] + 1);
-    }
+	Impl::Id advance(){
+		if (System::get_ids<T>().at(current_index) == max_id)
+			return max_id;
+		return advance(System::get_ids<T>().at(current_index) + 1);
+	}
 
-    Impl::Id advance(Impl::Id target){
-        while (System::get_ids<T>()[current_index] < target){
-            current_index++;
-        }
-        return System::get_ids<T>()[current_index];
-    }
-    operator bool(){
-        return System::get_ids<T>()[current_index] != max_id;
-    }
-    template<class U>
-    U &get(){
-        static_assert(std::is_same<U, T>::value, "Invalid type for this iterator");
-        return *(*this);
-    }
-    Impl::Id get_id(){
-        return System::get_ids<T>()[current_index];
-    }
-    std::vector<Impl::Id> get_ids() const{
-        return {System::get_ids<T>()[current_index]};
-    }
+	Impl::Id advance(Impl::Id target){
+		while (System::get_ids<T>().at(current_index) < target){
+			current_index++;
+		}
+		return System::get_ids<T>().at(current_index);
+	}
+	operator bool(){
+		return System::get_ids<T>().at(current_index) != max_id;
+	}
+	template<class U>
+	U &get(){
+		static_assert(std::is_same<U, T>::value, "Invalid type for this iterator");
+		return *(*this);
+	}
+	Impl::Id get_id(){
+		return System::get_ids<T>().at(current_index);
+	}
+	std::vector<Impl::Id> get_ids() const{
+		return {System::get_ids<T>().at(current_index)};
+	}
 
 private:
-    T &operator *(){
-        return System::get_components<T>()[current_index];
-    }
-    const T &operator *() const{
-        return System::get_components<T>()[current_index];
-    }
-    std::size_t current_index = 0;
+	T &operator *(){
+		return System::get_components<T>().at(current_index);
+	}
+	const T &operator *() const{
+		return System::get_components<T>().at(current_index);
+	}
+	std::size_t current_index = 0;
 };
 
 template <class T, class... Rest>
 struct System_iterator{
-    Impl::Id advance(){
-        return advance(t.get_id() + 1);
-    }
-    Impl::Id advance(Impl::Id target){
-        auto rest_target = rest.advance(target);
-        target = t.advance(rest_target);
-        while (target != rest_target){
-            if (target < rest_target)
-                target = t.advance(rest_target);
-            else
-                rest_target = rest.advance(target);
-        }
-        return target;
-    }
-    operator bool(){
-        return t;
-    }
+	Impl::Id advance(){
+		return advance(t.get_id() + 1);
+	}
+	Impl::Id advance(Impl::Id target){
+		target = t.advance(target);
+		for (auto new_target = rest.advance(target); target != new_target; new_target = rest.advance(target)){
+			target = t.advance(new_target);
+			if (target == new_target)
+				return target;
+		}
+		return target;
+		/*
+		auto rest_target = rest.advance(target);
+		while (target != rest_target){
+			if (target < rest_target)
+				target = t.advance(rest_target);
+			else
+				rest_target = rest.advance(target);
+		}
+		return target;
+		*/
+	}
+	operator bool(){
+		return t;
+	}
 //    std::tuple<T&, (Rest &)...>
 //    operator *(){
 //        return std::tie(*t, (*rest)...);
@@ -113,58 +152,67 @@ struct System_iterator{
 //    std::tuple<const T&, const Rest &...> operator *() const{
 //        return std::tie(*t, *rest);
 //    }
-    template<class U>
-    std::enable_if_t<std::is_same<U, T>::value, U&> get(){
-        return t.template get<U>();
-    }
-    template<class U>
-    std::enable_if_t<!std::is_same<U, T>::value, U&> get(){
-        return rest.template get<U>();
-    }
-    std::vector<Impl::Id> get_ids() const{
-        auto v = rest.get_ids();
-        v.push_back(t.get_id());
-        return v;
-    }
+	template<class U>
+	std::enable_if_t<std::is_same<U, T>::value, U&> get(){
+		return t.template get<U>();
+	}
+	template<class U>
+	std::enable_if_t<!std::is_same<U, T>::value, U&> get(){
+		return rest.template get<U>();
+	}
+	std::vector<Impl::Id> get_ids() const{
+		auto v = rest.get_ids();
+		v.push_back(t.get_id());
+		return v;
+	}
 
 private:
-    System_iterator<T> t;
-    System_iterator<Rest...> rest;
-    //std::tuple<Rest...> rest;
+	System_iterator<T> t;
+	System_iterator<Rest...> rest;
+	//std::tuple<Rest...> rest;
 };
 
 template <class... T>
 bool operator ==(const System_iterator<T...> &lhs, const System_iterator<T...> &rhs){
-    return lhs.current_index == rhs.current_index;
+	return lhs.current_index == rhs.current_index;
 }
 
 struct Entity
 {
-    Entity(){
-        id = id_counter++;
-    }
-    template<class Component>
-    void add(Component &&c){
-        auto &ids = System::get_ids<Component>();
-        auto &components = System::get_components<Component>();
-        auto insert_position = std::lower_bound(begin(ids), end(ids), id);
-        assert(*insert_position != id);
-        components.insert(begin(components) + (insert_position - begin(ids)), std::forward<Component>(c));
-        ids.insert(insert_position, id);
-    }
-    template<class Component>
-    Component *get(){
-        auto &ids = System::get_ids<Component>();
-        auto id_it = lower_bound(begin(ids), end(ids), id);
-        if (*id_it != id)
-            return nullptr;
-        auto pos = id_it - begin(ids);
-        auto &components = System::get_components<Component>();
-        return &components[pos];
-    }
+	Entity(){
+		id = id_counter++;
+	}
+	template<class Component>
+	void add(Component &&c){
+		auto &ids = System::get_ids<Component>();
+		auto &components = System::get_components<Component>();
+		auto insert_position = std::lower_bound(begin(ids), end(ids), id);
+		assert(*insert_position != id);
+		components.insert(begin(components) + (insert_position - begin(ids)), std::forward<Component>(c));
+		ids.insert(insert_position, id);
+	}
+	template<class Component, class... Args>
+	void emplace(Args &&...c){
+		auto &ids = System::get_ids<Component>();
+		auto &components = System::get_components<Component>();
+		auto insert_position = std::lower_bound(begin(ids), end(ids), id);
+		assert(*insert_position != id);
+		components.emplace(begin(components) + (insert_position - begin(ids)), std::forward<Args>(c)...);
+		ids.insert(insert_position, id);
+	}
+	template<class Component>
+	Component *get(){
+		auto &ids = System::get_ids<Component>();
+		auto id_it = lower_bound(begin(ids), end(ids), id);
+		if (*id_it != id)
+			return nullptr;
+		auto pos = id_it - begin(ids);
+		auto &components = System::get_components<Component>();
+		return &components.at(pos);
+	}
 private:
-    static Impl::Id id_counter;
-    Impl::Id id;
+	static Impl::Id id_counter;
+	Impl::Id id;
 };
 
 #endif // ENTITY_H
--- main.cpp
+++ main.cpp
@@ -17,215 +17,215 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "ARPGT1");
-    auto &now =  std::chrono::high_resolution_clock::now;
-    auto last_update_timepoint = now();
-    const auto lfps = 30;
-    const auto logical_frame_duration = std::chrono::milliseconds(1000) / lfps;
-    using namespace std::chrono_literals;
+	sf::RenderWindow window(sf::VideoMode(800, 600), "ARPGT1");
+	auto &now =  std::chrono::high_resolution_clock::now;
+	auto last_update_timepoint = now();
+	const auto lfps = 30;
+	const auto logical_frame_duration = std::chrono::milliseconds(1000) / lfps;
+	using namespace std::chrono_literals;
 
-    window.setVerticalSyncEnabled(true);
+	window.setVerticalSyncEnabled(true);
 
-    b2World world({0, 0});
+	b2World world({0, 0});
 
-    Player &p = Player::player;
-    p.set_window(&window);
-    {
+	Player &p = Player::player;
+	p.set_window(&window);
+	{
 
-        b2BodyDef bodyDef;
-        bodyDef.type = b2_dynamicBody;
-        bodyDef.position.Set(10, 10);
-        auto body = world.CreateBody(&bodyDef);
-        b2CircleShape characterShape;
-        characterShape.m_radius = 50 / 100.f;
+		b2BodyDef bodyDef;
+		bodyDef.type = b2_dynamicBody;
+		bodyDef.position.Set(10, 10);
+		auto body = world.CreateBody(&bodyDef);
+		b2CircleShape characterShape;
+		characterShape.m_radius = 50 / 100.f;
 
-        b2FixtureDef fixtureDef;
-        fixtureDef.shape = &characterShape;
-        fixtureDef.density = 1.f;
-        fixtureDef.friction = 0;
-        body->CreateFixture(&fixtureDef);
-        body->SetLinearDamping(10.f);
+		b2FixtureDef fixtureDef;
+		fixtureDef.shape = &characterShape;
+		fixtureDef.density = 1.f;
+		fixtureDef.friction = 0;
+		body->CreateFixture(&fixtureDef);
+		body->SetLinearDamping(10.f);
 
-        p.add(body);
-    }
-    p.camera.rotate(0);
-    PracticeDummy pd;
-    {
-        b2BodyDef bodyDef;
-        bodyDef.type = b2_dynamicBody;
-        bodyDef.position.Set(11, 10);
-        auto body = world.CreateBody(&bodyDef);
-        b2CircleShape characterShape;
-        characterShape.m_radius = 40 / 100.f;
+		p.add(std::move(body));
+	}
+	p.camera.rotate(0);
+	PracticeDummy pd;
+	{
+		b2BodyDef bodyDef;
+		bodyDef.type = b2_dynamicBody;
+		bodyDef.position.Set(11, 10);
+		auto body = world.CreateBody(&bodyDef);
+		b2CircleShape characterShape;
+		characterShape.m_radius = 40 / 100.f;
 
-        b2FixtureDef fixtureDef;
-        fixtureDef.shape = &characterShape;
-        fixtureDef.density = 1.f;
-        fixtureDef.friction = 0;
-        body->CreateFixture(&fixtureDef);
-        body->SetLinearDamping(5.0f);
+		b2FixtureDef fixtureDef;
+		fixtureDef.shape = &characterShape;
+		fixtureDef.density = 1.f;
+		fixtureDef.friction = 0;
+		body->CreateFixture(&fixtureDef);
+		body->SetLinearDamping(5.0f);
 
-        pd.add(body);
-        sf::CircleShape cs(characterShape.m_radius * 100);
-        cs.setOutlineColor({100, 200, 200});
-        pd.add(cs);
-    }
-    std::vector<Entity> zombies(1000);
-    int zcounter = 0;
-    for (auto &z : zombies){
-        b2BodyDef bodyDef;
-        bodyDef.type = b2_dynamicBody;
-        bodyDef.position.Set(zcounter % 100, -zcounter / 100);
-        zcounter++;
-        auto body = world.CreateBody(&bodyDef);
-        b2CircleShape characterShape;
-        characterShape.m_radius = 40 / 100.f;
+		pd.add(std::move(body));
+		sf::CircleShape cs(characterShape.m_radius * 100);
+		cs.setOutlineColor({100, 200, 200});
+		pd.add(std::move(cs));
+	}
+	std::vector<Entity> zombies(32);
+	int zcounter = 0;
+	for (auto &z : zombies){
+		b2BodyDef bodyDef;
+		bodyDef.type = b2_dynamicBody;
+		bodyDef.position.Set(zcounter % 100, -zcounter / 100);
+		zcounter++;
+		auto body = world.CreateBody(&bodyDef);
+		b2CircleShape characterShape;
+		characterShape.m_radius = 40 / 100.f;
 
-        b2FixtureDef fixtureDef;
-        fixtureDef.shape = &characterShape;
-        fixtureDef.density = 1.f;
-        fixtureDef.friction = 0;
-        body->CreateFixture(&fixtureDef);
-        body->SetLinearDamping(5.0f);
+		b2FixtureDef fixtureDef;
+		fixtureDef.shape = &characterShape;
+		fixtureDef.density = 1.f;
+		fixtureDef.friction = 0;
+		body->CreateFixture(&fixtureDef);
+		body->SetLinearDamping(5.0f);
 
-        z.add(body);
-        sf::CircleShape cs(characterShape.m_radius * 100);
-        cs.setFillColor({200, 0, 0});
-        z.add(cs);
-        z.add(Components::ZombieAi());
-    }
-    Entity background_picture;
-    {
-        sf::Sprite sprite;
-        background_picture.add(sf::Texture());
-        auto &image = *background_picture.get<sf::Texture>();
-        sprite.setPosition(-1000000, -1000000);
-        image.loadFromFile("grass3.png");
-        sprite.setTexture(image);
-        image.setRepeated(true);
-        image.setSmooth(true);
-        sprite.setTextureRect({0, 0, 2000000, 2000000});
-        background_picture.add(sprite);
-    }
+		z.add(std::move(body));
+		sf::CircleShape cs(characterShape.m_radius * 100);
+		cs.setFillColor({200, 0, 0});
+		z.add(std::move(cs));
+		z.emplace<Components::ZombieAi>();
+	}
+	Entity background_picture;
+	{
+		sf::Sprite sprite;
+		background_picture.emplace<sf::Texture>();
+		auto &image = *background_picture.get<sf::Texture>();
+		sprite.setPosition(-1000000, -1000000);
+		image.loadFromFile("grass3.png");
+		sprite.setTexture(image);
+		image.setRepeated(true);
+		image.setSmooth(true);
+		sprite.setTextureRect({0, 0, 2000000, 2000000});
+		background_picture.add(std::move(sprite));
+	}
 #if 0
-    sf::Font font;
-    (void)font;
+	sf::Font font;
+	(void)font;
 
-    if (!font.loadFromFile("Arctik 1.5.ttf"))
-        throw std::runtime_error("Missing font: Arctik 1.5.ttf");
-    sf::Text text;
-    text.setFont(font);
+	if (!font.loadFromFile("Arctik 1.5.ttf"))
+		throw std::runtime_error("Missing font: Arctik 1.5.ttf");
+	sf::Text text;
+	text.setFont(font);
 #endif
-    while (window.isOpen()){
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed){
-                p.set_window(nullptr);
-                window.close();
-            }
-            if (event.type == sf::Event::Resized)
-                //auto zoom = p.camera.get_zoom();
-                p.camera.set_size(event.size.width, event.size.height);
-                //p.camera.set_zoom(zoom);
-                //window.setSize({event.size.width, event.size.height});
-            if (event.type == sf::Event::MouseWheelMoved){
-                const auto zoom_factor = 0.1f;
-                p.camera.set_zoom(pow(1 - zoom_factor, event.mouseWheel.delta));
-            }
-        }
-        if (!window.isOpen())
-            continue;
-        //UpdateList::updateAll();
-        while (now() - last_update_timepoint > logical_frame_duration)
-        {
-            last_update_timepoint += logical_frame_duration;
-            auto &player_body = *p.get<Components::Physical_shape>();
-            //setting player direction and velocity
-            {
-                b2Vec2 vel{0, 0};
-                if (window.hasFocus()){
-                    const auto camera_turning_speed = 5.f; //in degree
-                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
-                        vel.y--;
-                    }
-                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
-                        vel.x--;
-                    }
-                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
-                        vel.y++;
-                    }
-                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
-                        vel.x++;
-                    }
-                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)){
-                        p.camera.rotate(-camera_turning_speed);
-                    }
-                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::E)){
-                        p.camera.rotate(camera_turning_speed);
-                    }
-                    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Tab)){
-                        const auto &dummy_body = *pd.get<Components::Physical_shape>();
-                        const auto &dummy_pos = dummy_body->GetPosition();
-                        const auto &player_pos = player_body->GetPosition();
-                        p.camera.face(dummy_pos.x - player_pos.x, dummy_pos.y - player_pos.y);
-                    }
-                    vel.Normalize();
-                    vel *= 5;
-                    Utility::rotate(vel.x, vel.y, p.camera.get_rotation() * M_PI / 180);
-                    //player_body->ApplyForce(vel, player_body->GetPosition(), true);
-                }
-                player_body->SetLinearVelocity(vel);
-            }
-            //resolve physics
-            world.Step(1/30.f, 6, 2);
-        }
-        window.clear(sf::Color::Black);
-        //rendering system
-        ZombieAI::ZombieAI_system::update();
-        for (auto sit = System::range<sf::Sprite>(); sit; sit.advance()){
-            window.draw(sit.get<sf::Sprite>());
-        }
-        auto &player_body = *p.get<Components::Physical_shape>();
-        const auto &pos = player_body->GetPosition();
-        p.camera.set_position(pos.x * 100, pos.y * 100);
-        for (auto sit = System::range<Components::CircleShape, Components::Physical_shape>(); sit; sit.advance()){
-            const auto &pos = sit.get<Components::Physical_shape>()->GetPosition();
-            auto &cs = sit.get<Components::CircleShape>();
-            auto sfmlpos = Utility::b2s_coords(pos);
-            sfmlpos.x -= cs.getRadius();
-            sfmlpos.y -= cs.getRadius();
-            cs.setPosition(sfmlpos);
-            window.draw(cs);
-        }
+	while (window.isOpen()){
+		// check all the window's events that were triggered since the last iteration of the loop
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// "close requested" event: we close the window
+			if (event.type == sf::Event::Closed){
+				p.set_window(nullptr);
+				window.close();
+			}
+			if (event.type == sf::Event::Resized)
+				//auto zoom = p.camera.get_zoom();
+				p.camera.set_size(event.size.width, event.size.height);
+				//p.camera.set_zoom(zoom);
+				//window.setSize({event.size.width, event.size.height});
+			if (event.type == sf::Event::MouseWheelMoved){
+				const auto zoom_factor = 0.1f;
+				p.camera.set_zoom(pow(1 - zoom_factor, event.mouseWheel.delta));
+			}
+		}
+		if (!window.isOpen())
+			continue;
+		//UpdateList::updateAll();
+		while (now() - last_update_timepoint > logical_frame_duration)
+		{
+			last_update_timepoint += logical_frame_duration;
+			auto &player_body = *p.get<Components::Physical_shape>();
+			//setting player direction and velocity
+			{
+				b2Vec2 vel{0, 0};
+				if (window.hasFocus()){
+					const auto camera_turning_speed = 5.f; //in degree
+					if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
+						vel.y--;
+					}
+					if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
+						vel.x--;
+					}
+					if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
+						vel.y++;
+					}
+					if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
+						vel.x++;
+					}
+					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)){
+						p.camera.rotate(-camera_turning_speed);
+					}
+					if (sf::Keyboard::isKeyPressed(sf::Keyboard::E)){
+						p.camera.rotate(camera_turning_speed);
+					}
+					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Tab)){
+						const auto &dummy_body = *pd.get<Components::Physical_shape>();
+						const auto &dummy_pos = dummy_body->GetPosition();
+						const auto &player_pos = player_body->GetPosition();
+						p.camera.face(dummy_pos.x - player_pos.x, dummy_pos.y - player_pos.y);
+					}
+					vel.Normalize();
+					vel *= 5;
+					Utility::rotate(vel.x, vel.y, p.camera.get_rotation() * M_PI / 180);
+					//player_body->ApplyForce(vel, player_body->GetPosition(), true);
+				}
+				player_body->SetLinearVelocity(vel);
+			}
+			//resolve physics
+			world.Step(1/30.f, 6, 2);
+		}
+		window.clear(sf::Color::Black);
+		//rendering system
+		ZombieAI::ZombieAI_system::update();
+		for (auto sit = System::range<sf::Sprite>(); sit; sit.advance()){
+			window.draw(sit.get<sf::Sprite>());
+		}
+		auto &player_body = *p.get<Components::Physical_shape>();
+		const auto &pos = player_body->GetPosition();
+		p.camera.set_position(pos.x * 100, pos.y * 100);
+		for (auto sit = System::range<Components::CircleShape, Components::Physical_shape>(); sit; sit.advance()){
+			const auto &pos = sit.get<Components::Physical_shape>()->GetPosition();
+			auto &cs = sit.get<Components::CircleShape>();
+			auto sfmlpos = Utility::b2s_coords(pos);
+			sfmlpos.x -= cs.getRadius();
+			sfmlpos.y -= cs.getRadius();
+			cs.setPosition(sfmlpos);
+			window.draw(cs);
+		}
 #if 0
-        {
-            static int fps;
-            fps++;
-            static auto starttime = std::chrono::high_resolution_clock::now();
-            const auto measure_time = std::chrono::milliseconds(500);
-            if (std::chrono::high_resolution_clock::now() - starttime < measure_time){
-                starttime += measure_time;
-                text.setString(std::to_string(fps*2));
-                std::cout << fps * 2;
-                fps = 0;
-            }
-            window.draw(text);
-        }
+		{
+			static int fps;
+			fps++;
+			static auto starttime = std::chrono::high_resolution_clock::now();
+			const auto measure_time = std::chrono::milliseconds(500);
+			if (std::chrono::high_resolution_clock::now() - starttime < measure_time){
+				starttime += measure_time;
+				text.setString(std::to_string(fps*2));
+				std::cout << fps * 2;
+				fps = 0;
+			}
+			window.draw(text);
+		}
 #endif
-        {
-            static int fps;
-            fps++;
-            static auto starttime = std::chrono::high_resolution_clock::now();
-            const auto measure_time = std::chrono::milliseconds(500);
-            if (std::chrono::high_resolution_clock::now() - starttime > measure_time){
-                starttime += measure_time;
-                window.setTitle(std::to_string(fps*2));
-                fps = 0;
-            }
-        }
-        window.display();
-    }
+		{
+			static int fps;
+			fps++;
+			static auto starttime = std::chrono::high_resolution_clock::now();
+			const auto measure_time = std::chrono::milliseconds(500);
+			if (std::chrono::high_resolution_clock::now() - starttime > measure_time){
+				starttime += measure_time;
+				window.setTitle(std::to_string(fps*2));
+				fps = 0;
+			}
+		}
+		window.display();
+	}
 }
