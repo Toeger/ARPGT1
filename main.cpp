c888a7b48b87183e4e729184654a2d9fbc9431c4
toeger <toeger@anonymouse.me>
2015-12-24 14:02:57 +0100

    cleaned up main and basically removed everything in the process

--- main.cpp
+++ main.cpp
@@ -17,8 +17,73 @@
 #include "Physics/circle.h"
 #include "Graphics/physicals.h"
 
-int main()
-{
+void handle_events(sf::RenderWindow &window, Player &p){
+	//check immediate action buttons
+	if (window.hasFocus()){
+		const auto camera_turning_speed = 5.f; //in degree
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)){
+			p.camera.rotate(-camera_turning_speed);
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::E)){
+			p.camera.rotate(camera_turning_speed);
+		}
+	}
+
+	//check all events that were triggered since the last iteration of the loop
+	sf::Event event;
+	while (window.pollEvent(event))
+	{
+		switch (event.type){
+		case sf::Event::Closed: //closed the window
+		{
+			p.set_window(nullptr);
+			window.close();
+		}
+		return;
+		case sf::Event::Resized:
+		{
+			//auto zoom = p.camera.get_zoom();
+			p.camera.set_size(event.size.width, event.size.height);
+			//p.camera.set_zoom(zoom);
+			//window.setSize({event.size.width, event.size.height});
+		}
+		break;
+		case sf::Event::MouseWheelMoved:
+		{
+			const auto zoom_factor = 0.1f;
+			p.camera.set_zoom(pow(1 - zoom_factor, event.mouseWheel.delta));
+		}
+		break;
+		default:
+		{
+		}
+		break;
+		}
+	}
+}
+
+void update_logical_frame(){
+	//TODO: movement, collision detection, ...
+}
+
+void render_frame(sf::RenderWindow &window){
+	window.clear(sf::Color::Black);
+	{
+		static int fps;
+		fps++;
+		static auto starttime = std::chrono::high_resolution_clock::now();
+		const auto measure_time = std::chrono::milliseconds(500);
+		if (std::chrono::high_resolution_clock::now() - starttime > measure_time){
+			starttime += measure_time;
+			window.setTitle("FPS: " + std::to_string(fps * 1000 / measure_time.count()));
+			fps = 0;
+		}
+	}
+	Graphics::draw_physicals(window);
+	window.display();
+}
+
+int main(){
 	Physical::Body body;
 	body.attach(Physical::Circle(Physical::Transformator::get_translation_matrix({20, 10}), 10), {0, 0}, {0, 0});
 
@@ -29,13 +94,13 @@ int main()
 		sf::Event event;
 		while (window.pollEvent(event));
 	}
+	window.setVerticalSyncEnabled(true);
+
 	auto &now =  std::chrono::high_resolution_clock::now;
 	auto last_update_timepoint = now();
-	const auto lfps = 30;
+	const auto lfps = 30; //logical frames per second, independent of graphical fps
 	const auto logical_frame_duration = std::chrono::milliseconds(1000) / lfps;
-	using namespace std::chrono_literals;
 
-	window.setVerticalSyncEnabled(true);
 
 	Player &p = Player::player;
 	p.set_window(&window);
@@ -44,127 +109,14 @@ int main()
 		b.attach(Physical::Circle(Physical::Transformator(), 10), {}, {});
 		p.add(std::move(b));
 	}
-	//p.camera.rotate(0);
-	//p.camera.set_size(40, 30);
-	//p.camera.set_position(0, 0);
-	//p.camera.set_zoom(20);
-
-	std::vector<Entity> balls(5);
-	{
-		sf::CircleShape c;
-		c.setFillColor(sf::Color::Red);
-		c.setRadius(1);
-		c.setPosition(-10, -10);
-		balls[0].add(c);
-		c.setPosition(-10, 10);
-		balls[1].add(c);
-		c.setPosition(10, 10);
-		balls[2].add(c);
-		c.setPosition(10, -10);
-		balls[3].add(c);
-		c.setPosition(5, 5);
-		c.setFillColor(sf::Color::Green);
-		balls[4].add(c);
-	}
 
 	while (window.isOpen()){
-		// check all the window's events that were triggered since the last iteration of the loop
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			// "close requested" event: we close the window
-			if (event.type == sf::Event::Closed){
-				p.set_window(nullptr);
-				window.close();
-			}
-			if (event.type == sf::Event::Resized)
-				//auto zoom = p.camera.get_zoom();
-				p.camera.set_size(event.size.width, event.size.height);
-				//p.camera.set_zoom(zoom);
-				//window.setSize({event.size.width, event.size.height});
-			if (event.type == sf::Event::MouseWheelMoved){
-				const auto zoom_factor = 0.1f;
-				p.camera.set_zoom(pow(1 - zoom_factor, event.mouseWheel.delta));
-			}
-			if (event.type == sf::Event::KeyPressed){
-				switch (event.key.code){
-				case sf::Keyboard::Num0: case sf::Keyboard::Num1: case sf::Keyboard::Num2: case sf::Keyboard::Num3:
-				{
-					auto &green_ball = *balls[4].get<sf::CircleShape>();
-					auto &red_ball = *balls[event.key.code - sf::Keyboard::Num0].get<sf::CircleShape>();
-					green_ball.setPosition(Physical::Transformator::get_rotation_matrix({2,1}, red_ball.getPosition()) * green_ball.getPosition());
-				}
-				break;
-				default:
-				break;
-				}
-			}
-		}
-		if (!window.isOpen())
-			continue;
-		//UpdateList::updateAll();
-		while (now() - last_update_timepoint > logical_frame_duration)
-		{
-			//resolve physics
+		while (now() - last_update_timepoint > logical_frame_duration){
 			last_update_timepoint += logical_frame_duration;
-			//setting player direction and velocity
-			{
-				if (window.hasFocus()){
-					const auto camera_turning_speed = 5.f; //in degree
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)){
-					}
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)){
-					}
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)){
-					}
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)){
-					}
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)){
-						p.camera.rotate(-camera_turning_speed);
-					}
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::E)){
-						p.camera.rotate(camera_turning_speed);
-					}
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Tab)){
-					}
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)){
-						//for now shoot a ball straight up
-
-					}
-					if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
-						auto &pos = balls[4].get<sf::CircleShape>()->getPosition();
-						Physical::Vector v{pos.x, pos.y};
-						Physical::Transformator t;
-						t.set_translation({0, 1});
-						v = t * v;
-						balls[4].get<sf::CircleShape>()->setPosition(v.x, v.y);
-					}
-					//player_body->ApplyForce(vel, player_body->GetPosition(), true);
-				}
-			}
-
+			update_logical_frame();
 		}
 		p.camera.set_position(0, 0);
-		window.clear(sf::Color::Black);
-		//rendering system
-		for (auto sit = System::range<sf::Sprite>(); sit; sit.advance()){
-			window.draw(sit.get<sf::Sprite>());
-		}
-		for (auto sit = System::range<sf::CircleShape>(); sit; sit.advance()){
-			window.draw(sit.get<sf::CircleShape>());
-		}
-		{
-			static int fps;
-			fps++;
-			static auto starttime = std::chrono::high_resolution_clock::now();
-			const auto measure_time = std::chrono::milliseconds(500);
-			if (std::chrono::high_resolution_clock::now() - starttime > measure_time){
-				starttime += measure_time;
-				window.setTitle("FPS: " + std::to_string(fps*2));
-				fps = 0;
-			}
-		}
-		Graphics::draw_physicals(window);
-		window.display();
+		render_frame(window);
+		handle_events(window, p);
 	}
 }
