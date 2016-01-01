a71380e4dd094e23b76c536b395cdab311c15bd6
toeger <toeger@anonymouse.me>
2016-01-01 01:10:17 +0100

    removed weapon include that was removed last commit

--- player.h
+++ player.h
@@ -3,7 +3,6 @@
 
 #include <SFML/Graphics.hpp>
 #include "camera.h"
-#include "weapon.h"
 #include "entity.h"
 #include "Physics/physics_utility.h"
 
--- practicedummy.h
+++ practicedummy.h
@@ -2,14 +2,13 @@
 #define PRACTICEDUMMY_H
 
 #include <SFML/Graphics.hpp>
-#include "weapon.h"
 #include "entity.h"
 
 class PracticeDummy : public Entity
 {
 public:
-    PracticeDummy();
-    sf::CircleShape character;
+	PracticeDummy();
+	sf::CircleShape character;
 };
 
 #endif // PRACTICEDUMMY_H
