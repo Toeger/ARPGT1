56421417df4529e279d81aff4989d4d115486959
toeger <toeger@anonymouse.me>
2016-01-01 01:00:44 +0100

    wrote collision function for exact circle-circle collision, but the result is incorrect

--- Physics/collision.h
+++ Physics/collision.h
@@ -4,9 +4,9 @@
 #include "circle.h"
 
 namespace Physical {
-	bool collides(const Physical::Circle &c1, const Transformator &t1, const Physical::Circle &c2, const Transformator &t2){
-		auto distance = t1.get_translation() - t2.get_translation();
-		return distance.x * distance.x + distance.y * distance.y < c1.radius * c1.radius + c2.radius * c2.radius;
+	inline bool collides(const Physical::Circle &c1, const Transformator &t1, const Physical::Circle &c2, const Transformator &t2){
+		auto distance = t1.vector - t2.vector;
+		return distance.x * distance.x + distance.y * distance.y < (c1.radius + c2.radius) * (c1.radius + c2.radius);
 	}
 }
 
