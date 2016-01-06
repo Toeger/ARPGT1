45ed20772fca9ea0c315b5e723b56d0850b3b904
toeger <toeger@anonymouse.me>
2016-01-06 15:46:04 +0100

    changed float comparison a bit more, still not good

--- Tests/tests_utility.h
+++ Tests/tests_utility.h
@@ -2,25 +2,28 @@
 #define TESTS_UTILITY_H
 
 #include <cassert>
-
-inline void assert_equal(float a, float b){
-	assert(a - b < 0.0005);
-}
-
-inline void assert_equal(int a, float b){
-	assert_equal(a * 1.f, b);
-}
-
-inline void assert_equal(float a, int b){
-	assert_equal(a, b * 1.f);
-}
+#include <cmath>
+#include <type_traits>
 
 template <class T, class U>
-void assert_equal(const T &a, const U &b){
+std::enable_if_t<!std::is_floating_point<T>::value && !std::is_floating_point<U>::value>
+assert_equal(const T &a, const U &b){
 	//this function exists for debugging purposes
 	//assert(foo() == bar()); will not let me examine the return values of foo() and bar(),
 	//while this function captures references to the return values
 	assert(a == b);
+}
+
+//special cases for float comparisons
+template <class T, class U>
+std::enable_if_t<std::is_floating_point<T>::value || std::is_floating_point<U>::value>
+assert_equal(const T &a, const U &b){
+	if (std::abs(b) < 1){
+		assert(a - b < 0.00005);
+	}
+	else{
+		assert(std::abs(a / b) - 1 < 0.000005);
+	}
 }
 
 #endif // TESTS_UTILITY_H
