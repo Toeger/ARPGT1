4b490cdd773d993ba306715381ecc557c35f6d7b
toeger <toeger@anonymouse.me>
2016-01-22 12:48:41 +0100

    everything broken

--- Physics/body.h
+++ Physics/body.h
@@ -15,145 +15,67 @@
 #include "collision.h"
 
 namespace Physical{
-	//add types here to allow attaching them to a body
-	//all attachable types must be listed here
-	using Supported_types = std::tuple<Physical::Circle, Physical::Line>;
+	//add shapes here that a Body can be
+	using Supported_types = std::tuple<Physical::Circle, Physical::Line>; //TODO: add Physical::Polygon
 	static const constexpr std::size_t number_of_supported_types = std::tuple_size<Supported_types>::value;
 
-	namespace Helper{
-		//from https://stackoverflow.com/a/18063608/3484570
-		//this is used to get the index for a type in a tuple
-		//example: Index<int, std::tuple<float, char, int, double>>::value returns 2 at compile time
-		//if the type does not exist you get a compilation error
-		//if the type exists multiple times you get the first index with a match
-		template <class T, class Tuple>
-		struct Index;
+	namespace { //helper
+			//from https://stackoverflow.com/a/18063608/3484570
+			//this is used to get the index for a type in a tuple
+			//example: Index<int, std::tuple<float, char, int, double>>::value returns 2 at compile time
+			//if the type does not exist you get a compilation error
+			//if the type exists multiple times you get the first index with a match
+			template <class T, class Tuple>
+			struct Index;
 
-		template <class T, class... Types>
-		struct Index<T, std::tuple<T, Types...>> {
-			static const std::size_t value = 0;
-		};
+			template <class T, class... Types>
+			struct Index<T, std::tuple<T, Types...>> {
+				static const std::size_t value = 0;
+			};
 
-		template <class T, class U, class... Types>
-		struct Index<T, std::tuple<U, Types...>> {
-			static const std::size_t value = 1 + Index<T, std::tuple<Types...>>::value;
-		};
-
-		//this essentially declares a "template<class T> std::vector<T> my_vectors;", but that is not allowed in C++14
-		//instead we end up with this clusterfuck:
-		template <class ...Types>
-		struct VectorHolder;
-		template <>
-		struct VectorHolder<>{};
-		template <class T, class ...Rest>
-		struct VectorHolder<T, Rest...>{
-			template <class U>
-			std::enable_if_t<std::is_same<T, U>::value, std::vector<std::pair<U, Transformator>> &>
-			get(){
-				return data;
-			}
-			template <class U>
-			std::enable_if_t<!std::is_same<T, U>::value, std::vector<std::pair<U, Transformator>> &>
-			get(){
-				return rest.template get<U>();
-			}
-
-			template <class U>
-			std::enable_if_t<std::is_same<T, U>::value, const std::vector<std::pair<U, Transformator>> &>
-			get() const{
-				return data;
-			}
-			template <class U>
-			std::enable_if_t<!std::is_same<T, U>::value, const std::vector<std::pair<U, Transformator>> &>
-			get() const{
-				return rest.template get<U>();
-			}
-			VectorHolder() = default;
-			VectorHolder(VectorHolder &&) = default;
-			VectorHolder(const VectorHolder &) = delete;
-			VectorHolder &operator =(const VectorHolder &) = delete;
-			VectorHolder &operator =(VectorHolder &&) = default;
-			std::vector<std::pair<T, Transformator>> data;
-			struct VectorHolder<Rest...> rest;
-		};
-
-		template <class>
-		struct VectorHolderFromTuple;
-		template <class ...Types>
-		struct VectorHolderFromTuple<std::tuple<Types...>>{
-			using type = VectorHolder<Types...>;
-		};
+			template <class T, class U, class... Types>
+			struct Index<T, std::tuple<U, Types...>> {
+				static const std::size_t value = 1 + Index<T, std::tuple<Types...>>::value;
+			};
 	}
-
-	//A physical body consists of basic physical objects such as circles and polygons
-	//you create a physical body by attaching multiple such primitives to it by specifying the offset vector and direction vector
-	class Body
+	//A DynamicBody consists of one of the supported shapes with collision detection and a Transformator that translates and rotates the shape
+	template <class Shape>
+	class DynamicBody
 	{
+		static_assert(Index<Shape, Supported_types>::value >= 0, //comparison always true, but we only care about if it compiles, not the actual value
+					  "Unsupported type for DynamicBody, add it to Physical::Supported_types to fix");
 	public:
 		//special member functions
-		Body(const Transformator &transformator)
+		DynamicBody(Shape &&shape, const Transformator &transformator)
 			:current_transformator(transformator)
 			,next_transformator(transformator)
+			,shape(std::move(shape))
 		{}
-		Body(const Vector &position, const Direction &direction)
-			:current_transformator(position, direction)
-			,next_transformator(current_transformator)
+		DynamicBody(const Vector &position = {}, const Direction &direction = {})
+			:DynamicBody(Transformator{position, direction})
 		{}
-		Body() = default;
-		Body(const Vector &position) : Body(position, {}){}
-		Body(const Direction &direction) : Body({}, direction){}
-		Body(Body &&) = default;
-		Body(const Body &) = delete;
-		Body &operator =(const Body &other) = delete;
-		Body &operator =(Body &&other) = default;
-
-		//end_frame is called after all physics has been resolved and we switch from one logical frame to the next
-		static void end_frame(){
-			for (auto r = ECS::System::range<Body>(); r; r.advance()){
-				auto &body = r.get<Body>();
-				body.current_transformator = body.next_transformator;
-				body.update_aabb();
-			}
-		}
-
-		//attach an object to this Body with the given offset and Direction. The attached object will move when the body moves and take part in collision of the body.
-		template <class T>
-		void attach(T &&t, Vector offset = Vector{}, Direction direction = Direction{}){
-			static_assert(Helper::Index<T, Supported_types>::value < 1000000, "Requesting unsupported type"); //just need to instantiate the template, comparison doesn't matter
-			auto &attached_vector = attached_objects.get<Utility::remove_cvr<T>>();
-			Transformator transformator{offset, direction};
-			aabb.combine(AABB(t, current_transformator + transformator));
-			attached_vector.emplace_back(std::make_pair<Utility::remove_cvr<T>, Transformator>(std::forward<T>(t), std::move(transformator)));
-		}
-
-		//apply a function f to all objects attached to this body, iterating over all types of attached objects
-		//there must be an overload of f for every type in Physical::Supported_types
-		template <class Function>
-		void apply(Function &&f){
-			apply<0>(f);
-		}
-		template <class Function>
-		void apply(Function &&f) const{
-			apply<0>(f);
-		}
+		DynamicBody(const Direction &direction)
+			:DynamicBody({}, direction)
+		{}
+		DynamicBody(DynamicBody &&) = default;
+		DynamicBody(const DynamicBody &) = delete;
+		DynamicBody &operator =(const DynamicBody &other) = delete;
+		DynamicBody &operator =(DynamicBody &&other) = default;
 
 		//operators
-		Body &operator +=(const Vector &vector){
+		DynamicBody &operator +=(const Vector &vector){
 			auto new_transformator = next_transformator + vector;
 			if (!colliding(new_transformator)){
 				next_transformator = new_transformator;
 			}
 			return *this;
 		}
-		Body &operator +=(const Direction &direction){
+		DynamicBody &operator +=(const Direction &direction){
 			auto new_transformator = next_transformator + direction;
 			if (!colliding(new_transformator)){
 				next_transformator = new_transformator;
 			}
 			return *this;
-		}
-		const AABB &get_aabb() const{
-			return aabb;
 		}
 		//a Body holds 2 transformators: the current one where the object is and the next one where it will be next frame
 		const Transformator &get_current_transformator() const{
@@ -162,105 +84,49 @@ namespace Physical{
 		const Transformator &get_next_transformator() const{
 			return next_transformator;
 		}
+
+		AABB get_aabb() const{
+			return AABB(shape, current_transformator);
+		}
+
+		const Shape &get_shape() const{
+			return shape;
+		}
 	private:
-		//overloads of apply:
-		//this overload of apply applies f to a specific type T that gets explicitly specified
-		template<class T, class Function>
-		void apply(Function &&f){
-			for (auto &ao : attached_objects.get<Utility::remove_cvr<T>>()){
-				f(ao.first, current_transformator + ao.second);
-			}
-		}
-		template<class T, class Function>
-		void apply(Function &&f) const{
-			for (auto &ao : attached_objects.get<Utility::remove_cvr<T>>()){
-				f(ao.first, current_transformator + ao.second);
-			}
-		}
-		//this overload ends iterating over the types
-		template <std::size_t type_number, class Function>
-		std::enable_if_t<type_number == number_of_supported_types>
-		apply(Function &&)
-		{}
-		template <std::size_t type_number, class Function>
-		std::enable_if_t<type_number == number_of_supported_types>
-		apply(Function &&) const
-		{}
-		//this overload applies f to Supported_types[type_number] and recursively applies f to all following type_numbers
-		template <std::size_t type_number, class Function>
-		std::enable_if_t<type_number < number_of_supported_types>
-		apply(Function &&f){
-			apply<decltype(std::get<type_number>(std::declval<Supported_types>()))>(f);
-			apply<type_number + 1>(f);
-		}
-		template <std::size_t type_number, class Function>
-		std::enable_if_t<type_number < number_of_supported_types>
-		apply(Function &&f) const{
-			apply<decltype(std::get<type_number>(std::declval<Supported_types>()))>(f);
-			apply<type_number + 1>(f);
-		}
-
+		template <size_t type_index = 0>
 		bool colliding(const Transformator &new_transformator){
-			auto new_aabb = get_aabb(new_transformator);
-			for (auto r = ECS::System::range<Body>(); r; r.advance()){
-				auto &other = r.get<Body>();
-				if (&other == this)
+			using Shape_type = std::tuple_element_t<type_index, Supported_types>;
+			using Body_type = DynamicBody<Shape_type>;
+			for (auto r = ECS::System::range<Body_type>(); r; r.advance()){
+				//auto &other = r.get<Body_type>();
+				auto &other = r.get();
+				if (&other == this) //can we optimize the branch out somehow? //TODO: at least if the type of *this != Body_type we can skip the check
 					continue;
-				if (Physical::collides(other.aabb, new_aabb)){
-					if (collides(other, *this, new_transformator - current_transformator)){
-						return true;
-					}
-				}
+				if (collides(other, *this, new_transformator - current_transformator))
+					return true;
 			}
-			return false;
+			return colliding<type_index + 1>();
 		}
-		AABB get_aabb(const Transformator &t) const{
-			AABB aabb;
-			apply([&aabb, &t](auto &type, const Transformator &){
-				aabb.combine(AABB(type, t));
-			});
-			return aabb;
-		}
-		void update_aabb(){
-			aabb.clear();
-			apply([this](auto &type, const Transformator &t){
-				aabb.combine(AABB(type, t));
-			});
-		}
-
-		static bool collides(const Body &b1, const Body &b2){
-			bool collided = false;
-			b1.apply([&collided, &b2](const auto &obj1, const Transformator &t1){
-				if (!collided){
-					b2.apply([&collided, &obj1, &t1](const auto obj2, const Transformator &t2){
-						if (!collided){
-							collided = Physical::collides(obj1, t1, obj2, t2);
-						}
-					});
-				}
-			});
-			return collided;
-		}
-		static bool collides(const Body &b1, const Body &b2, const Transformator &offset){
-			//checks collision, except that the transformator for b2 has the given offset from b2.current_transformator()
-			bool collided = false;
-			b1.apply([&collided, &b2, &offset](const auto &obj1, const Transformator &t1){
-				if (!collided){
-					b2.apply([&collided, &obj1, &t1, &offset](const auto obj2, const Transformator &t2){
-						if (!collided){
-							collided = Physical::collides(obj1, t1, obj2, offset + t2);
-						}
-					});
-				}
-			});
-			return collided;
-		}
-		//storing attached objects
-		Helper::VectorHolderFromTuple<Supported_types>::type attached_objects;
-		AABB aabb;
 		//a Body holds 2 transformators: the current one where the object is and the next one where it will be next frame
 		Transformator current_transformator, next_transformator;
+		Shape shape;
 	};
+	//end_frame is called after all physics has been resolved and we switch from one logical frame to the next
+	namespace {
+		template <size_t type_index = 0>
+		void end_frame(){
+
+		}
+		template <>
+		void end_frame<number_of_supported_types>()
+		{}
+	}
+	inline void end_frame(){
+//		for (auto r = ECS::System::range<DynamicBody>(); r; r.advance()){
+//			auto &body = r.get<DynamicBody>();
+//			body.current_transformator = body.next_transformator;
+//		}
+	}
 }
 
 #endif // BODY_H
--- Physics/sensor.h
+++ Physics/sensor.h
@@ -6,20 +6,20 @@
 namespace Physical{
 	//a sensor is essentially the same as a Body, except you cannot collide with it
 	//instead a sensor will "activate" when a Body is in its range
-	struct Sensor : private Body{
-		Sensor(const Transformator &transformator)
-			:Body(transformator)
-		{}
-		using Body::attach;
-		using Body::operator +=;
-		template <class Function>
-		void apply(Function &&f){
-			Body::apply(f);
-		}
-		const Physical::AABB &get_aabb() const{
-			return Body::get_aabb();
-		}
-	};
+//	struct Sensor : private Body{
+//		Sensor(const Transformator &transformator)
+//			:Body(transformator)
+//		{}
+//		using Body::attach;
+//		using Body::operator +=;
+//		template <class Function>
+//		void apply(Function &&f){
+//			Body::apply(f);
+//		}
+//		const Physical::AABB &get_aabb() const{
+//			return Body::get_aabb();
+//		}
+//	};
 }
 
 #endif // SENSOR_H
