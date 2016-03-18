34943bbd31d8926bf6dcf0a18c269e976071b663
toeger <toeger@anonymouse.me>
2016-03-18 01:03:52 +0100

    first design of skill structure

--- GamePlay/skill.h
+++ GamePlay/skill.h
@@ -1,10 +1,53 @@
 #ifndef SKILL_H
 #define SKILL_H
 
+#include "Graphics/common_graphics_components.h"
+#include "ECS/entity.h"
 
-namespace Skill
+#include <string>
+#include <bitset>
+
+namespace Skills
 {
+	void load_skills(const std::string &file_path);
+	enum class Type{
+		projectile,
+		aura,
+		instant,
+	};
+	enum Collisions{
+		enemies,
+		allies,
+		self,
+		map,
+		size
+	};
 
+	struct Skill{
+		void activate();
+		void cancel();
+		std::string name;
+		float channeltime = 0.f;
+		float executiontime = 0.f;
+		float cooldown = 0.f;
+		float size = 0.f;
+		int time = 0;
+		Common_components::Animated_model animation;
+		std::bitset<Targets::size> collisions;
+		ECS::Entity_handle owner;
+		Skill *effect = nullptr;
+		Type type; //tag that defines the content of data
+		union{
+			struct Projectile{
+				ECS::Entity_handle target;
+				float turnspeed; //in degrees per tick
+				float speed = 0.f;
+			};
+			struct Aura{
+
+			};
+		} data;
+	};
 }
 
 #endif // SKILL_H
